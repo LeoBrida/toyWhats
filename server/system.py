e81af1fc410c346d7fa7bdfc89e50c6e7b2db279
Leonardo De Brida <leonardo.brida2000@gmail.com>
2023-09-29 18:31:29 -0300

    Login, ServerMenu

--- server/system.py
+++ server/system.py
@@ -6,7 +6,7 @@ from user_managment import User
 
 
 # ---------------------Dados de usuários (simulado em memória) ----------------
-global users_database
+#global users_database # não precisa definir como global por que ela nao ta dentro de nenhuma função
 users_database = []
 
 # ------------------------- Registro do usuário -------------------------------
@@ -41,102 +41,102 @@ def login():
     login = input("Digite seu login: ")
     password = input("Digite sua senha: ")
 
-    global userToLogin #Não pode ser global, tem que ser local do método
-    userToLogin = User
-
     for c in range(len(users_database)):
         person = users_database[c]
-
         if person.login == login:
             userToLogin = person
-        elif userToLogin == None and c == len(users_database):
-            print("Usuário não encontrado.")
 
-    # Hasheando a senha recebida com scrypt e comparando com a que tem guardada
-    # salt_received_password = generate_salt()      essa variável era assim. Mudei pq só assim as senhas batem. O salt precisa ser diferente nesse caso?
-    salt_received_password = userToLogin.salt
-    scrypt_received_password = derive_key_scrypt(salt_received_password, password)
-    if userToLogin.password == scrypt_received_password:
-        # Segundo fator de autenticação, usar o TOTP
-        #secret_key -> getotp -> retorna numero (user e servidor)
-        #comparar numero gerado pelo usuario e pelo servidor, se for o mesmo, ta autenticado
-        user_secret = generate_2fa_code(userToLogin.secret_key)
-        server_secret = generate_2fa_code(userToLogin.secret_key)
-        
-        if user_secret == server_secret:
-            print(f"Bem-vindo, {userToLogin.login}!")
-        else:
-            print("Autenticação falhou!")
-    else:
-        print("Senha incorreta.")
+            # Hasheando a senha recebida com scrypt e comparando com a que tem guardada
+            salt_received_password = userToLogin.salt # salt_received_password = generate_salt()   
+            scrypt_received_password = derive_key_scrypt(salt_received_password, password)
 
-def list_users_data():
-    print("Login\t")
-    print("Celular\t")
-    print("Scrypt Senha\t")
-    print("Salt\n")
+            if userToLogin.password == scrypt_received_password:
+                # Segundo fator de autenticação
+                user_secret = generate_2fa_code(userToLogin.secret_key)
+                server_secret = generate_2fa_code(userToLogin.secret_key)
 
-    for user in users_database:
-        print(f"{user.login}\t")
-        print(f"{user.phone}\t")
-        print(f"{user.password}\t")
-        print(f"{user.salt}\n")
+                if user_secret == server_secret:
+                    print(f"\nBem-vindo, {userToLogin.login}!")
+                    #return # a partir daqui ele deve ir para o server_menu()
+                else:
+                    print("\nAutenticação falhou!")
+            else:
+                print("\nSenha incorreta.")
 
-    voltar = input("Digite 0 (zero) para voltar")
-    if voltar == 0:
-        menu()
+        else: #person == None and c == len(users_database)
+            print("\nUsuário não encontrado.")
 
 # ---------------------- Escolher o usuário --------------------------
 def choose_user():
     print("Para enviar uma mensagem você precisa escolher um usuário")
-    for person in users_database:
-        print(f"{person.login}\n")
+    for user in users_database:
+        print(f"{user.login}\n")
 
-    login = input("Escreva o nome do usuário que deseja enviar uma mensagem:")
+    login = input("Escreva o nome do usuário que deseja enviar uma mensagem: ")
 
     for user in users_database:
         if user.login == login:
             return user
 
-def menu():
+
+'''
+Percebi que a gente precisa de um menu geral e um menu para o servidor.
+E o menu do servidor só pode ser acessado depois do login
+'''
+# ---------------------- Menu de entrada --------------------------
+
+def entry_menu():
     print("\n1. Cadastro")
     print("2. Login")
-    print("3. Enviar mensagem")
-    print("4. Sair")
-    print("5. Listar usuários")
-    print("6. Comando especial")
+    print("3. Sair")
     option = input("Escolha uma opção: ")
-
     return option
 
+# ---------------------- Menu do servidor --------------------------
+def server_menu():
+    print("1. Enviar mensagem")
+    print("2. Comando especial")
+    print("3. Sair")
+    option = input("Escolha uma opção: ")
+    return option
+
+# ---------------------- Mostrar dados dos usuários --------------------------
 def showUsersData():
     for user in users_database:
-        print("\n" + "login: " + user.login)
+        print("\nlogin: " + user.login)
         print("phone: " + user.phone)
         print("password: " + str(user.password))
         print("salt: " + str(user.salt))
         print("secret_key: " + user.secret_key)
 
+    back = input("\nDigite 0 (zero) para voltar: ")
+    if back == 0:
+        server_menu()
+
+
 # Menu principal
 while True:
-    option = menu()
-
+    option = entry_menu()
     if option == "1":
         register_user()
     elif option == "2":
         login()
     elif option == "3":
-        choose_user()
-    elif option == "4":
         break
-    elif option == "5":
-        list_users_data()
-    elif option == "6":
-        showUsersData()
     else:
         print("Opção inválida. Tente novamente.")
 
 
-
-
+'''
+if server == True:
+            option = server_menu()
+            if option == "1":
+                choose_user()
+            elif option == "2":
+                showUsersData()
+            elif option == "3":
+                entry_menu()
+            else:
+                print("Opção inválida. Tente novamente.")
+'''
 
